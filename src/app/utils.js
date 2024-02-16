203272f92dbfae4621d143f52fa72f9d4f5d88d0
Rolf Matela <rolffreitas@yahoo.com.br>
2024-02-16 10:17:28 -0300

    feat: criação do arquivo utils para a home page
    arquivo contendo alguns componentes que são gerados dentro da home page da aplicação

---
+++ src/app/utils.js
@@ -0,0 +1,14 @@
+import { Header } from '@/components/header';
+import { Footer } from '@/components/footer';
+
+export function HeaderGenerator() {
+    return (
+        <Header />
+    );
+}
+
+export function FooterGenerator() {
+    return (
+        <Footer />
+    );
+}
