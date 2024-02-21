e367197f5f548cda0bdf4bc64c28cffa2e8bc9f7
Rolf Matela <rolffreitas@yahoo.com.br>
2024-02-21 00:20:30 -0300

    feat: cria componente Table
    criação do componente da tabela dividido em dois arquivos principais, columns e dataTable

--- src/components/table/columns.js
+++ src/components/table/columns.js
@@ -1,12 +1,49 @@
+'use client';
+import { useState } from "react";
+import axios from "axios";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter
+} from "@/components/ui/dialog"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogDescription,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
 const columns = [
     {
         accessorKey: 'marca',
-        header: () => 'Marca',
-        cell: info => info.getValue()
+        header: () => <div className="w-[150px]">Ração</div>,
+        cell: info => <div className="w-[150px] font-semibold">{info.getValue()}</div>
     },
     {
         accessorKey: 'quantidade',
-        header: () => 'Quantidade',
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    <div className="w-[100px]">Quantidade</div>
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
         cell: ({ row }) => {
             const quantidade = parseFloat(row.getValue("quantidade"));
             const formatted = new Intl.NumberFormat("pt-BR", {
@@ -14,12 +51,34 @@ const columns = [
                 unit: "kilogram"
             }).format(quantidade)
 
-            return <div className="text-right font-medium">{formatted}</div>
+            return <div className="text-center w-[100px]">{formatted}</div>
+        }
+    },
+    {
+        accessorKey: 'data',
+        header: ({ column }) => {
+            return (
+                <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    <div className="w-[150px]">Data da compra</div>
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const dateString = row.getValue("data");
+            const [year, month, day] = dateString.split("-");
+            const data = new Date(year, month - 1, day);
+            const formatted = data.toLocaleDateString("pt-BR");
+
+            return <div className="text-center w-[150px]">{formatted}</div>
         }
     },
     {
         accessorKey: 'valor',
-        header: () => <div className="text-right">Valor</div>,
+        header: () => <div className="w-[100px] text-right">Valor</div>,
         cell: ({ row }) => {
             const valor = parseFloat(row.getValue("valor"));
             const formatted = new Intl.NumberFormat("pt-BR", {
@@ -27,7 +86,120 @@ const columns = [
                 currency: "BRL"
             }).format(valor)
 
-            return <div className="text-right font-medium">{formatted}</div>
+            return <div className="w-[100px] text-right">{formatted}</div>
+        }
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const tableRow = row.original;
+            const [novaMarca, setNovaMarca] = useState(tableRow.marca);
+            const [novaQuantidade, setNovaQuantidade] = useState(tableRow.quantidade);
+            const [novaData, setNovaData] = useState(tableRow.data);
+            const [novoValor, setNovoValor] = useState(tableRow.valor);
+
+            const handleSave = async () => {
+                const updatedData = {
+                    marca: novaMarca,
+                    quantidade: novaQuantidade,
+                    data: novaData,
+                    valor: novoValor,
+                    id: tableRow._id
+                }
+
+                await axios.patch('http://localhost:3001/racao', updatedData);
+            }
+
+            return (
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Alterar informações da compra</DialogTitle>
+                            <DialogDescription>
+                                Faça alterações sobre os dados inseridos.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="marca" className="text-right">
+                                    Marca
+                                </Label>
+                                <Input
+                                    id="marca"
+                                    type="text"
+                                    value={novaMarca}
+                                    onChange={(e) => setNovaMarca(e.target.value)}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="quantidade" className="text-right">
+                                    Quantidade
+                                </Label>
+                                <Input
+                                    id="quantidade"
+                                    type="number"
+                                    value={novaQuantidade}
+                                    onChange={(e) => setNovaQuantidade(e.target.value)}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="data" className="text-right">
+                                    Data
+                                </Label>
+                                <Input
+                                    id="data"
+                                    type="date"
+                                    value={novaData}
+                                    onChange={(e) => setNovaData(e.target.value)}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="username" className="text-right">
+                                    Valor
+                                </Label>
+                                <Input
+                                    id="valor"
+                                    type="number"
+                                    value={novoValor}
+                                    onChange={(e) => setNovoValor(e.target.value)}
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <Input
+                                type="hidden"
+                                name="id"
+                                value={tableRow._id}
+                            />
+                        </div>
+                        <DialogFooter>
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    <Button type="submit" onClick={handleSave}>Salvar alterações</Button>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>Alterações salvas com sucesso</AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            As alterações foram realizadas e salvas, clique em continuar para retornar a página.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogAction onClick={() => window.location.reload()}>Continuar</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            )
         }
     }
 ]
--- src/components/table/dataTable.js
+++ src/components/table/dataTable.js
@@ -1,58 +1,176 @@
-"use client";
-
-import { columns } from "./columns";
-
+'use client';
+import { useState, useEffect } from "react";
 import {
     flexRender,
     getCoreRowModel,
-    useReactTable
+    useReactTable,
+    getPaginationRowModel,
+    getSortedRowModel
 } from "@tanstack/react-table";
-
 import {
     Table,
     TableBody,
     TableCell,
+    TableFooter,
     TableHead,
     TableHeader,
     TableRow
 } from "@/components/ui/table"
+import { Button } from "../ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "../ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton"
 
-const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-})
+export default function DataTable({ columns, data }) {
+    const [sorting, setSorting] = useState([]);
+    const [filtroMes, setFiltroMes] = useState(null);
+    const [dadosFiltrados, setDadosFiltrados] = useState(null);
 
-return (
-    <div className="p-2">
-        <Table>
-            <TableHeader>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <TableHead key={header.id}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                            </TableHead>
-                        ))}
+    useEffect(() => {
+        if (filtroMes) {
+            const filtrandoDados = data.filter(item => {
+                const date = new Date(item.data);
+                const mesAno = date.toLocaleString('pt-BR', { month: 'long', timeZone: 'UTC' });
+                const mesAnoUpper = mesAno.charAt(0).toUpperCase() + mesAno.slice(1);
+                return mesAnoUpper === filtroMes;
+            });
+            setDadosFiltrados(filtrandoDados);
+        } else {
+            setDadosFiltrados(data)
+        }
+    }, [filtroMes, data])
+
+    const table = useReactTable({
+        data: dadosFiltrados ? dadosFiltrados : data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        initialState: {
+            pagination: {
+                pageIndex: 0,
+                pageSize: 5
+            }
+        },
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            sorting
+        }
+    })
+
+    if (!dadosFiltrados) {
+        return (
+            <div className="flex flex-col space-y-3 p-1">
+                <Skeleton className="h-[121px] w-[842px] rounded-xl" />
+                <div className="flex flex-row-reverse space-x-4 space-x-reverse p-1">
+                    <Skeleton className="h-[40px] w-[73px]" />
+                    <Skeleton className="h-[40px] w-[73px]" />
+                    <Skeleton className="h-[40px] w-[73px]" />
+                </div>
+            </div>
+        )
+    }
+
+    const filtrarPorMes = (mes) => {
+        setFiltroMes(mes);
+    };
+
+    const mesesDisponiveis = [...new Set(data.map(item => {
+        const date = new Date(item.data);
+        const month = date.toLocaleString('pt-BR', { month: 'long', timeZone: 'UTC' });
+        return month.charAt(0).toUpperCase() + month.slice(1);
+    }))];
+
+    const calcularTotal = () => {
+        const total = dadosFiltrados.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue.valor), 0);
+
+        return new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(total);
+    };
+
+    return (
+        <div className="p-2">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <TableHead key={header.id} className={`w-${header.width}`}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </TableHead>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows.map((row) => (
+                        <TableRow key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <TableCell key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))
+                    }
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={4}>Total</TableCell>
+                        <TableCell className="w-[150px] text-right">
+                            {calcularTotal()}
+                        </TableCell>
                     </TableRow>
-                ))}
-            </TableHeader>
-            <TableBody>
-                {table.getRowModel().rows.map(row => (
-                    <TableRow key={row.id}>
-                        {row.getVisibleCells().map(cell => (
-                            <TableCell key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </div>
-)
+                </TableFooter>
+            </Table>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline">
+                            Filtrar
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>Meses</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            {mesesDisponiveis.map((mes, index) => (
+                                <DropdownMenuItem key={index} onClick={() => filtrarPorMes(mes)}>{mes}</DropdownMenuItem>
+                            ))}
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Anterior
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Próxima
+                </Button>
+            </div>
+        </div>
+    )
+}
