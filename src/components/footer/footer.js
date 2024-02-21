7e7d71db0185742ab5d6ea624a992cb10c8b2519
Rolf Matela <rolffreitas@yahoo.com.br>
2024-02-21 00:15:23 -0300

    refactor: pequena alteração no estilo
    alteração realizada no estilo do footer, adicionando uma borda superior

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full border-b border-border/40">
+        <footer className="w-full border-t border-border/40">
             <div className="container flex h-14 max-w-screen items-center">
                 <span className="hidden sm:inline-block">trademark temporária.</span>
             </div>
