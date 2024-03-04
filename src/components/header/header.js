433b6f66a92d72af455416aecfc2f4844c9a826c
Rolf Matela <rolffreitas@yahoo.com.br>
2024-03-04 14:57:31 -0300

    refactor: edita componente Header
    edição do componente Header da aplicação

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,9 @@
 export default function Header() {
-    return (
-        <header className="w-full border-b border-border/40">
-            <div className="container flex h-14 max-w-screen items-center">
-                <span className="hidden font-bold sm:inline-block">Zeus</span>
-            </div>
-        </header>
-    );
+  return (
+    <header className='bg-[#334E68] w-full border-b border-border/40'>
+      <div className='container flex h-14 max-w-screen items-center'>
+        <span className='text-white hidden font-bold sm:inline-block'>PawCash</span>
+      </div>
+    </header>
+  );
 }
