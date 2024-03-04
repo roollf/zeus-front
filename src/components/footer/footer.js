924cc765388d12593e06cea1419fe7d5db6db25b
Rolf Matela <rolffreitas@yahoo.com.br>
2024-03-04 14:55:29 -0300

    refactor: edita componente de Footer
    edição do componente de Footer da aplicação

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,11 @@
 export default function Footer() {
-    return (
-        <footer className="w-full border-t border-border/40">
-            <div className="container flex h-14 max-w-screen items-center">
-                <span className="hidden sm:inline-block">trademark temporária.</span>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className='bg-[#334E68] w-full border-t border-border/40'>
+      <div className='container flex h-14 max-w-screen items-center'>
+        <span className='text-white hidden sm:inline-block'>
+          Cuide do Seu Melhor Amigo com Cada Centavo.
+        </span>
+      </div>
+    </footer>
+  );
 }
